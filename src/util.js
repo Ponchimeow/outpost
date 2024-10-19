c46323d8fddd0ee6f1a1ec0651f7b8170aade693
ponchimeow <trancetoradora@gmail.com>
2024-10-19 10:09:00 +0800

    feat: getNumberIntervals function

--- src/util.js
+++ src/util.js
@@ -2,3 +2,43 @@ export const addComma = (num) => {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 
+export const getNumberIntervals = (rangeList = []) => {
+  rangeList.sort((a, b) => a[0] - b[0]);
+
+  const overlap = [];
+  const merged = [];
+  let prev = rangeList[0];
+
+  for (let i = 1; i < rangeList.length; i++) {
+    const curr = rangeList[i];
+
+    // overlap
+    if (curr[0] <= prev[1]) {
+      const startOverlap = Math.max(prev[0], curr[0]);
+      const endOverlap = Math.min(prev[1], curr[1]);
+      overlap.push([startOverlap, endOverlap]);
+
+      prev = [prev[0], Math.max(prev[1], curr[1])];
+    } else {
+      merged.push(prev);
+      prev = curr;
+    }
+  }
+  merged.push(prev);
+
+  const notInclude = [];
+  let start = 0;
+
+  for (const [low, high] of merged) {
+    if (start < low) {
+      notInclude.push([start, low - 1]);
+    }
+    start = high + 1;
+  }
+
+  if (start <= 20) {
+    notInclude.push([start, 20]);
+  }
+
+  return { overlap, notInclude };
+};
