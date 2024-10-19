3be339da9017c6d60b7e86dfe5b45362e65bb1fb
ponchimeow <trancetoradora@gmail.com>
2024-10-19 11:10:51 +0800

    feat: provider, adjust app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { getNumberIntervals } from "./util";
+import { Box } from "@chakra-ui/react";
+import PriceInput from "./components/PriceInput";
 
 function App() {
+  // useEffect(() => {
+  //   getNumberIntervals();
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Box>
+        <PriceInput label="入住費用" subLabel="每人每晚" currency="TWD" />
+      </Box>
     </div>
   );
 }
